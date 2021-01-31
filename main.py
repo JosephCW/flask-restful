7f770826c6ecf10aef98e586e485dfdb501fa559
Joseph Watts <josephcw@live.com>
2021-01-31 15:19:43 -0800

    Copy example to take URL parameters

--- main.py
+++ main.py
@@ -6,5 +6,14 @@ app = Flask(__name__)
 def root():
     return 'Root Directory'
 
+# Can provde URL parameters via type:name
+@app.route('/<int:number>/')
+def incrementer(number):
+    return f'Incremented number is {number+1}'
+
+@app.route('/<string:name>/')
+def hello(name):
+    return f'Hello, {name}!'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8445)
